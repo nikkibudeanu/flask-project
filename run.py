447fa9cb0dee76e26a88502540352f7ae04e69c5
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-15 17:48:21 +0000

    render html using flask

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import flask
+import os
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,4 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+if __name__ =="__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
